f40b5ff9f76611373d239bb81cd4d60b5903ab86
yash pal singh <123yashpalrathore@gmail.com>
2022-09-08 04:18:42 +0530

    changes done

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args) {
         int a = 12;
         int b =22;
-        System.out.println("the sum of two number"+ a+b);
+        System.out.println("the sum of two number is"+ a+b);
     }
 }
